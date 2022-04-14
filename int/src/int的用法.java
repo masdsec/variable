b4ede177c572e4caf4498a4a9c4d122e4a1cd0c5
Mr.沉鱼 <99015417+maspsec@users.noreply.github.com>
2022-04-14 18:39:42 +0800

    Add files via upload

---
+++ int/src/int的用法.java
@@ -0,0 +1,12 @@
+// 在这个程序中，我们先使用main方法打出与Helloworld相似的格式（可参考我的其他储蓄库，库名“Hello_World-java”）
+public class  int的用法{
+    public int的用法() {
+
+    }
+    public static void main(String[] args) {
+        //然后，我们使用变量int，然后给变量（鞋盒）命名，我命名的是“n”，然后变量里储蓄的是“0”（也就是鞋子是0）
+        int n  = 0;
+        //最后我们声明变量
+     System.out.println(n);
+    }
+}
