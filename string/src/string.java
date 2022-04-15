c61ad50e35874a99f8061744e6d0cda7b4f625c9
Mr.沉鱼 <99015417+maspsec@users.noreply.github.com>
2022-04-15 16:32:45 +0800

    Add files via upload

---
+++ string/src/string.java
@@ -0,0 +1,2 @@
+public class string {
+}
---
+++ string/src/string变量.java
@@ -0,0 +1,9 @@
+//先写一个main方法。
+public class string变量 {
+    public  static void main(String[] args){
+        //然后把变量string，命名为“y”，然后让它储蓄文本“masdsec”
+        String y = "masdsec";
+        //最后在声明变量
+        System.out.println("hi,good afternoon "+ y );
+    }
+}
